9bf5b03af1ac4f2182db0247a94a2e1974394957
gaussmouse <98789124+gaussmouse@users.noreply.github.com>
2023-03-01 11:29:03 -0800

    Modified route to test FarmsPage endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@
 // Components of the program are found in /components
 
 // TODO: Convert mobile "StyleSheet" inside react-components to .css file or start fresh
-import './App.css';
+//import './App.css';
 import * as React from 'react';
 // Use Route and Routes to define different application routes
 import { Route, Routes } from "react-router-dom";
 // Import needed components for routings
 //import Wrapper from './reactComponents/Wrapper';
 import FarmsPage from "./components/FarmsPage";
-//import FarmsMap from "./reactComponents/FarmsMap";
+import FarmsMap from "./components/FarmsMap";
 
 // Application component routing
 // Home page is a map with an overlaid search bar
@@ -19,10 +19,12 @@ const App = () => {
   return (
     <div>
         <Routes>
-          <Route path="/" element={<FarmsPage />}/>
+          <Route path="/" element={<FarmsMap />}/>
         </Routes>
     </div>
   )
 }
+
+// <Route path="/" element={<FarmsPage />}/>
 
 export default App;
