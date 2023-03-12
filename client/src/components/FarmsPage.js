b2d239eb559df7e9eba4ea8526232edbe5c1b496
purvisr <purvisr@oregonstate.edu>
2023-03-11 19:51:23 -0800

    fixed missing data on the graphs, added link back to map page

--- client/src/components/FarmsPage.js
+++ client/src/components/FarmsPage.js
@@ -11,15 +11,19 @@ import { futurePrecipitation } from "../futurePrecipitationData";
 //import Graphs from "./Graphs";
 import ReactDOM from 'react-dom';
 import { VictoryBar, VictoryLegend, VictoryLine, VictoryChart, VictoryAxis, VictoryLabel } from 'victory';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 let farmID = "";
+
 
 const FarmDetails = () => {
   farmID = useParams();
   farmID = farmID.id;
 }
 
+const backTab = () => {
+    window.location.href='/'
+}
 // Component Example
 const CurrDataTab = () => {
   FarmDetails();
@@ -53,14 +57,28 @@ const PastDataTab = () => {
   "1974", "1975", "1976", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992",
   "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", 
   "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"];
-  const celsiusMaxTemps = Object.values(historicMaxTemp[farmID]);
+  const celsiusMaxTemps = Object.values(historicMaxTemp[farmID-1]);
   const fahrenheitMaxTemps = celsiusMaxTemps.map(temp => (temp * 9/5) + 32);
 
-  const celsiusMinTemps = Object.values(historicMinTemp[farmID]);
+  const celsiusMinTemps = Object.values(historicMinTemp[farmID-1]);
   const fahrenheitMinTemps = celsiusMinTemps.map(temp => (temp * 9/5) + 32);
+  console.log(Object.values(hPrecipitation[farmID-1])[0]);
   return (
     <div>
-      <VictoryChart maxDomain={{ y: 2000, x: 101}} height={150} width={340}>
+      <VictoryChart 
+        maxDomain={{ y: 2300, x: 101}} 
+        minDomain={{ y: 500 }}
+        height={150} 
+        width={340}
+        style={{ 
+        parent: { 
+          maxWidth: 1200,
+          maxHeight: 500, 
+          minWidth: 250, 
+          minHeight: 100, 
+        } 
+      }}
+        >
       <VictoryLabel 
         text="Precipitation Over Time" 
         x={170} 
@@ -81,6 +99,8 @@ const PastDataTab = () => {
           />
         <VictoryAxis dependentAxis
           label="Precipitation (mm)"
+          tickCount={8}
+          offsetX={49.3}
           style={{
             grid: { stroke: "#e0e0e0", strokeWidth: 1 },
             axisLabel: {padding: 25, fontSize: 8},
@@ -88,10 +108,22 @@ const PastDataTab = () => {
             ticks: {stroke: "grey", size: 5}
             }}
           />
-        <VictoryBar width={50} style={{data: {fill: "#66ccff"}}} data={Object.values(hPrecipitation[farmID])} />
+        <VictoryBar width={50} style={{data: {fill: "#66ccff"}}} data={[Object.values(hPrecipitation[farmID-1])[0], ...Object.values(hPrecipitation[farmID-1])]} />
       </VictoryChart>
  
-      <VictoryChart maxDomain={{ y: 75, x: 101}} minDomain={{ y: 25, x: 0}} height={150} width={340}>
+      <VictoryChart 
+        maxDomain={{ y: 75, x: 101}} 
+        minDomain={{ y: 25, x: 0}} 
+        height={150} width={340}
+        style={{ 
+        parent: { 
+          maxWidth: 1200,
+          maxHeight: 500, 
+          minWidth: 250, 
+          minHeight: 100, 
+        } 
+      }}
+        >
       <VictoryLabel 
         text="Temperature Over Time" 
         x={170} 
@@ -101,11 +133,11 @@ const PastDataTab = () => {
         style={{ fontSize: 10 }}
         />
         <VictoryLine 
-          data={fahrenheitMaxTemps}
+          data={[fahrenheitMaxTemps[0], ...fahrenheitMaxTemps]}
           style={{data: {stroke: "red", strokeWidth: 0.25}}}
           />
         <VictoryLine 
-          data={fahrenheitMinTemps}
+          data={[fahrenheitMinTemps[0], ...fahrenheitMinTemps]}
           style={{data: {stroke: "blue", strokeWidth: 0.25}}}
           />
          <VictoryLegend x={295} y={50}
@@ -154,14 +186,30 @@ const FutureClimateGraphTab = () => {
   FarmDetails();
   const futureLabels = ["2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030", "2031", "2032", "2033", "2034", "2035", "2036",
   "2037", "2038", "2039", "2040", "2041", "2042", "2043", "2044", "2045", "2046", "2047", "2048", "2049", "2050"];
-  const celsiusMaxTemps = Object.values(futureMaxTemp[farmID]);
+  //const fLabels = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042,
+  //2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050];
+  const celsiusMaxTemps = Object.values(futureMaxTemp[farmID-1]);
   const fahrenheitMaxTemps = celsiusMaxTemps.map(temp => (temp * 9/5) + 32);
+  console.log(Object.values(futurePrecipitation[farmID-1]));
 
-  const celsiusMinTemps = Object.values(futureMinTemp[farmID]);
+  const celsiusMinTemps = Object.values(futureMinTemp[farmID-1]);
   const fahrenheitMinTemps = celsiusMinTemps.map(temp => (temp * 9/5) + 32);
   return (
     <div>
-      <VictoryChart maxDomain={{ y: 1500, x: 29}} height={150} width={340}>
+      <VictoryChart
+       maxDomain={{ y: 1500, x: 29}}
+       minDomain={{ y: 950}}
+       height={150} 
+       width={340}
+       style={{ 
+        parent: { 
+          maxWidth: 1200,
+          maxHeight: 500, 
+          minWidth: 250, 
+          minHeight: 100, 
+        } 
+      }}
+       >
       <VictoryLabel 
         text="Precipitation Over Time" 
         x={170} 
@@ -182,6 +230,8 @@ const FutureClimateGraphTab = () => {
           />
         <VictoryAxis dependentAxis
           label="Precipitation (mm)"
+          tickCount={15}
+          offsetX={48}
           style={{
             grid: { stroke: "#e0e0e0", strokeWidth: 1 },
             axisLabel: {padding: 25, fontSize: 8},
@@ -189,10 +239,23 @@ const FutureClimateGraphTab = () => {
             ticks: {stroke: "grey", size: 5}
             }}
           />
-        <VictoryBar width={50} style={{data: {fill: "#66ccff"}}} data={Object.values(futurePrecipitation[farmID])} />
+        <VictoryBar width={50} style={{data: {fill: "#66ccff"}}} data={[Object.values(futurePrecipitation[farmID-1])[0], ...Object.values(futurePrecipitation[farmID-1])]} />
       </VictoryChart>
  
-      <VictoryChart maxDomain={{ y: 75, x: 29}} minDomain={{ y: 25, x: 0}} height={150} width={340}>
+      <VictoryChart 
+        maxDomain={{ y: 75, x: 29}} 
+        minDomain={{ y: 25, x: 0}} 
+        height={150} 
+        width={340}
+        style={{ 
+        parent: { 
+          maxWidth: 1200,
+          maxHeight: 500, 
+          minWidth: 250, 
+          minHeight: 100, 
+        } 
+      }}
+        >
       <VictoryLabel 
         text="Temperature Over Time" 
         x={170} 
@@ -202,11 +265,11 @@ const FutureClimateGraphTab = () => {
         style={{ fontSize: 10 }}
         />
         <VictoryLine 
-          data={fahrenheitMaxTemps}
+          data={[fahrenheitMaxTemps[0], ...fahrenheitMaxTemps]}
           style={{data: {stroke: "red", strokeWidth: 0.25}}}
           />
         <VictoryLine 
-          data={fahrenheitMinTemps}
+          data={[fahrenheitMinTemps[0], ...fahrenheitMinTemps]}
           style={{data: {stroke: "blue", strokeWidth: 0.25}}}
           />
          <VictoryLegend x={295} y={50}
@@ -253,7 +316,7 @@ const FutureClimateGraphTab = () => {
 
 // Tabs structure Array
 const tabs = [
-  {
+    {
     label: 'Current Data', // Tab Title - String
     Component: CurrDataTab // Tab Body - JSX.Element
   },
@@ -268,6 +331,10 @@ const tabs = [
   {
     label: 'Future Climate Graphs',
     Component: FutureClimateGraphTab
+  },
+  {
+    label: "Back to Map",
+    Component: backTab
   }
 ]
 
