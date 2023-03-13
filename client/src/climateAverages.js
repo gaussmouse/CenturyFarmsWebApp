4621e8c9207ccec149c330395a3c72c12bdb1946
purvisr <purvisr@oregonstate.edu>
2023-03-13 11:25:44 -0700

    fixed typo

--- client/src/climateAverages.js
+++ client/src/climateAverages.js
@@ -4,475 +4,475 @@ export const futureMaxTemp = [
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1070
+      "meanPrecipitation": 1070
     },
     {
       "farmID": 2,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1144
+      "meanPrecipitation": 1144
     },
     {
       "farmID": 3,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.3,
       "meanMinTemp": 5,
-      "meanPercipitation": 1114
+      "meanPrecipitation": 1114
     },
     {
       "farmID": 4,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1072
+      "meanPrecipitation": 1072
     },
     {
       "farmID": 5,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1129
+      "meanPrecipitation": 1129
     },
     {
       "farmID": 6,
       "meanTemp": 12.2,
       "meanMaxTemp": 20.4,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1198
+      "meanPrecipitation": 1198
     },
     {
       "farmID": 7,
       "meanTemp": 12,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 5,
-      "meanPercipitation": 1079
+      "meanPrecipitation": 1079
     },
     {
       "farmID": 8,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.8,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1110
+      "meanPrecipitation": 1110
     },
     {
       "farmID": 9,
       "meanTemp": 11.5,
       "meanMaxTemp": 19.6,
       "meanMinTemp": 4.4,
-      "meanPercipitation": 1332
+      "meanPrecipitation": 1332
     },
     {
       "farmID": 10,
       "meanTemp": 12,
       "meanMaxTemp": 20,
       "meanMinTemp": 5,
-      "meanPercipitation": 1054
+      "meanPrecipitation": 1054
     },
     {
       "farmID": 11,
       "meanTemp": 10.9,
       "meanMaxTemp": 19,
       "meanMinTemp": 3.9,
-      "meanPercipitation": 1538
+      "meanPrecipitation": 1538
     },
     {
       "farmID": 12,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1084
+      "meanPrecipitation": 1084
     },
     {
       "farmID": 13,
       "meanTemp": 11.5,
       "meanMaxTemp": 19.5,
       "meanMinTemp": 4.6,
-      "meanPercipitation": 1249
+      "meanPrecipitation": 1249
     },
     {
       "farmID": 14,
       "meanTemp": 10.8,
       "meanMaxTemp": 19,
       "meanMinTemp": 3.9,
-      "meanPercipitation": 1536
+      "meanPrecipitation": 1536
     },
     {
       "farmID": 15,
       "meanTemp": 11.7,
       "meanMaxTemp": 19.7,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1119
+      "meanPrecipitation": 1119
     },
     {
       "farmID": 16,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1082
+      "meanPrecipitation": 1082
     },
     {
       "farmID": 17,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1096
+      "meanPrecipitation": 1096
     },
     {
       "farmID": 18,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 5,
-      "meanPercipitation": 1098
+      "meanPrecipitation": 1098
     },
     {
       "farmID": 19,
       "meanTemp": 11.7,
       "meanMaxTemp": 19.7,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1245
+      "meanPrecipitation": 1245
     },
     {
       "farmID": 20,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1085
+      "meanPrecipitation": 1085
     },
     {
       "farmID": 21,
       "meanTemp": 12,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 5,
-      "meanPercipitation": 1098
+      "meanPrecipitation": 1098
     },
     {
       "farmID": 22,
       "meanTemp": 10.9,
       "meanMaxTemp": 19.1,
       "meanMinTemp": 3.6,
-      "meanPercipitation": 1327
+      "meanPrecipitation": 1327
     },
     {
       "farmID": 23,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 5,
-      "meanPercipitation": 1087
+      "meanPrecipitation": 1087
     },
     {
       "farmID": 24,
       "meanTemp": 12,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1076
+      "meanPrecipitation": 1076
     },
     {
       "farmID": 25,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1083
+      "meanPrecipitation": 1083
     },
     {
       "farmID": 26,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1034
+      "meanPrecipitation": 1034
     },
     {
       "farmID": 27,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1033
+      "meanPrecipitation": 1033
     },
     {
       "farmID": 28,
       "meanTemp": 11.8,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1171
+      "meanPrecipitation": 1171
     },
     {
       "farmID": 29,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1031
+      "meanPrecipitation": 1031
     },
     {
       "farmID": 30,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.8,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1068
+      "meanPrecipitation": 1068
     },
     {
       "farmID": 31,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1149
+      "meanPrecipitation": 1149
     },
     {
       "farmID": 32,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1097
+      "meanPrecipitation": 1097
     },
     {
       "farmID": 33,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1098
+      "meanPrecipitation": 1098
     },
     {
       "farmID": 34,
       "meanTemp": 12,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1084
+      "meanPrecipitation": 1084
     },
     {
       "farmID": 35,
       "meanTemp": 11.1,
       "meanMaxTemp": 19.2,
       "meanMinTemp": 4.1,
-      "meanPercipitation": 1359
+      "meanPrecipitation": 1359
     },
     {
       "farmID": 36,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1022
+      "meanPrecipitation": 1022
     },
     {
       "farmID": 37,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1091
+      "meanPrecipitation": 1091
     },
     {
       "farmID": 38,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1146
+      "meanPrecipitation": 1146
     },
     {
       "farmID": 39,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1017
+      "meanPrecipitation": 1017
     },
     {
       "farmID": 40,
       "meanTemp": 11.9,
       "meanMaxTemp": 19.8,
       "meanMinTemp": 5,
-      "meanPercipitation": 1070
+      "meanPrecipitation": 1070
     },
     {
       "farmID": 41,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.8,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1080
+      "meanPrecipitation": 1080
     },
     {
       "farmID": 42,
       "meanTemp": 12,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 5,
-      "meanPercipitation": 1076
+      "meanPrecipitation": 1076
     },
     {
       "farmID": 43,
       "meanTemp": 11.5,
       "meanMaxTemp": 19.4,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1176
+      "meanPrecipitation": 1176
     },
     {
       "farmID": 44,
       "meanTemp": 11.2,
       "meanMaxTemp": 19.3,
       "meanMinTemp": 4.2,
-      "meanPercipitation": 1259
+      "meanPrecipitation": 1259
     },
     {
       "farmID": 45,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 5,
-      "meanPercipitation": 1081
+      "meanPrecipitation": 1081
     },
     {
       "farmID": 46,
       "meanTemp": 11.7,
       "meanMaxTemp": 19.8,
       "meanMinTemp": 4.6,
-      "meanPercipitation": 1081
+      "meanPrecipitation": 1081
     },
     {
       "farmID": 47,
       "meanTemp": 12,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 985
+      "meanPrecipitation": 985
     },
     {
       "farmID": 48,
       "meanTemp": 11.7,
       "meanMaxTemp": 19.7,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1112
+      "meanPrecipitation": 1112
     },
     {
       "farmID": 49,
       "meanTemp": 11.2,
       "meanMaxTemp": 19.4,
       "meanMinTemp": 4.2,
-      "meanPercipitation": 1278
+      "meanPrecipitation": 1278
     },
     {
       "farmID": 50,
       "meanTemp": 12,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1085
+      "meanPrecipitation": 1085
     },
     {
       "farmID": 51,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1078
+      "meanPrecipitation": 1078
     },
     {
       "farmID": 52,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1092
+      "meanPrecipitation": 1092
     },
     {
       "farmID": 53,
       "meanTemp": 11.8,
       "meanMaxTemp": 20,
       "meanMinTemp": 4.6,
-      "meanPercipitation": 1092
+      "meanPrecipitation": 1092
     },
     {
       "farmID": 54,
       "meanTemp": 11.5,
       "meanMaxTemp": 19.5,
       "meanMinTemp": 4.5,
-      "meanPercipitation": 1236
+      "meanPrecipitation": 1236
     },
     {
       "farmID": 55,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1092
+      "meanPrecipitation": 1092
     },
     {
       "farmID": 56,
       "meanTemp": 12,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1087
+      "meanPrecipitation": 1087
     },
     {
       "farmID": 57,
       "meanTemp": 11.7,
       "meanMaxTemp": 19.6,
       "meanMinTemp": 4.7,
-      "meanPercipitation": 1145
+      "meanPrecipitation": 1145
     },
     {
       "farmID": 58,
       "meanTemp": 11.9,
       "meanMaxTemp": 20.1,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1085
+      "meanPrecipitation": 1085
     },
     {
       "farmID": 59,
       "meanTemp": 12,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1083
+      "meanPrecipitation": 1083
     },
     {
       "farmID": 60,
       "meanTemp": 12.4,
       "meanMaxTemp": 20.6,
       "meanMinTemp": 5.2,
-      "meanPercipitation": 1088
+      "meanPrecipitation": 1088
     },
     {
       "farmID": 61,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 5,
-      "meanPercipitation": 1098
+      "meanPrecipitation": 1098
     },
     {
       "farmID": 62,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.2,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1263
+      "meanPrecipitation": 1263
     },
     {
       "farmID": 63,
       "meanTemp": 11.5,
       "meanMaxTemp": 19.5,
       "meanMinTemp": 4.5,
-      "meanPercipitation": 1364
+      "meanPrecipitation": 1364
     },
     {
       "farmID": 64,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1090
+      "meanPrecipitation": 1090
     },
     {
       "farmID": 65,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1091
+      "meanPrecipitation": 1091
     },
     {
       "farmID": 66,
       "meanTemp": 11.9,
       "meanMaxTemp": 20,
       "meanMinTemp": 5,
-      "meanPercipitation": 1144
+      "meanPrecipitation": 1144
     },
     {
       "farmID": 67,
       "meanTemp": 12.1,
       "meanMaxTemp": 20.3,
       "meanMinTemp": 4.9,
-      "meanPercipitation": 1161
+      "meanPrecipitation": 1161
     },
     {
       "farmID": 68,
       "meanTemp": 11.8,
       "meanMaxTemp": 19.9,
       "meanMinTemp": 4.8,
-      "meanPercipitation": 1053
+      "meanPrecipitation": 1053
     }
   ]
