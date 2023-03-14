7daddcda19fb33f82e6f9bac400e9ddef05ef20c
gaussmouse <98789124+gaussmouse@users.noreply.github.com>
2023-03-13 18:57:20 -0700

    Updated port number to be flexible

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ mongoose.connect(connection_string, {
 //    const mapboxPrivateToken = response.json();
 //    res.json({mapboxPrivateToken});
 //})
-
-app.listen(5000, ()=>{
-    console.log('Server is running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
 })
