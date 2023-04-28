699e120711ad9a080893c8f608fbb282b881a5d0
gaussmouse <98789124+gaussmouse@users.noreply.github.com>
2023-04-27 23:23:10 -0700

    Cleaned up indendation, comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 // App behaves as the navigation wrapper
 // Components of the program are found in /components
-
-// TODO: Convert mobile "StyleSheet" inside react-components to .css file or start fresh
-//import './App.css';
 import * as React from 'react';
 /// mapbox styling
 import 'mapbox-gl/dist/mapbox-gl.css';
 // Use Route and Routes to define different application routes
 import { Route, Routes } from "react-router-dom";
 // Import needed components for routings
-//import Wrapper from './components/Wrapper';
 import FarmPage from "./components/FarmsPage";
 import FarmsMap from "./components/FarmsMap";
 
 // Application component routing
 // Home page is a map with an overlaid search bar
-// Completed: routing to individual farms
 const App = () => {
   return(
     <Routes>
-        <Route path="/" element={<FarmsMap />}/> 
-        <Route path="/farms/:id" element={<FarmPage />}/>
+      <Route path="/" element={<FarmsMap />}/> 
+      <Route path="/farms/:id" element={<FarmPage />}/>
     </Routes>
   )
 }
