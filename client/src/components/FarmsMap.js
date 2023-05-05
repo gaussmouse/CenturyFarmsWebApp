967ad495bdfe48f0e0640c75ba441c7d757c9b17
gaussmouse <98789124+gaussmouse@users.noreply.github.com>
2023-05-04 21:19:49 -0700

    Added livestock, crop, farmCurrent data for filter

--- client/src/components/FarmsMap.js
+++ client/src/components/FarmsMap.js
@@ -6,8 +6,8 @@ import SearchBar from "../components/sidebar/SearchBar";
 // Plugins
 import ClickAwayListener from '@mui/base/ClickAwayListener';
 
-// Styleshees
-import "./stylesheets/inputs/multi-select.css"
+// Stylesheets
+import "./stylesheets/inputs/sidebar.css"
 
 // public access key - rotated periodically
 mapboxgl.accessToken = "add token here"; 
@@ -24,10 +24,18 @@ export default function FarmsMap () {
   // search
   //const [searchedFarms, setSearchedFarms] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [currentFilters, setFilterQuery] = useState([]);
-  const [multiSelectExpanded, setMultiSelectExpanded] = useState(false);
+  const [currentCropFilters, setCropFilterQuery] = useState([]);
+  const [currentLivestockFilters, setLivestockFilterQuery] = useState([]);
+  const [cropMultiSelectExpanded, setCropMultiSelectExpanded] = useState(false);
+  const [livestockMultiSelectExpanded, setLivestockMultiSelectExpanded] = useState(false);
   const [cropFilters, setCropFilters] = useState([]);
   const [livestockFilters, setLivestockFilters] = useState([]);
+  const [crops, setCrops] = useState([]);
+  //const [validCrops, setValidCrops] = useState([]);
+  const [livestock, setLivestock] = useState([]);
+  //const [validLivestock, setValidLivestock] = useState([]);
+  const [currentFarmInfo, setCurrentFarmInfo] = useState([]);
+  const [validFarms, setValidFarms] = useState([]);
  
   // -----------------------------------------------------------------------------
   /*
@@ -59,33 +67,80 @@ export default function FarmsMap () {
         center: [lng, lat],
         zoom: zoom
       });
-      const farmData = await getFarmLocations();       
-      setFarms(farmData);
+      const farmData = await getFarmLocations();  
+      setFarms(farmData);  
       mapFarmLocations(farmData);
-      //const cropData = await getCropFilters();
-      //setCropFilters(cropData);
 
-      //const livestockData = await getLivestockFilters();
-      //setLivestockFilters(livestockData);
+      const cropFilters = await getCropFilters();
+      const livestockFilters = await getLivestockFilters();
+      setCropFilters(cropFilters);
+      setLivestockFilters(livestockFilters);
       
+      const crops = await getCrops();
+      const livestock = await getLivestock();
+      setCrops(crops);
+      setLivestock(livestock);
+
+      const currentFarmInfo = await getCurrentFarmInfo();
+      setCurrentFarmInfo(currentFarmInfo);
     }
     initializeMap();
     map.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
   }, [lng, lat, zoom]);
 
-  /*
+  
   useEffect(() => {
-    const filters = encodeURIComponent(JSON.stringify(currentFilters));
+    // If currentFilters not null, fetch livestock types & crop types w/IDs that match
+    if (currentCropFilters === null && currentLivestockFilters === null) return;
+    //console.log(currentCropFilters);
+    //console.log(currentLivestockFilters);
+    
+    // Fetch crop & livestock IDs that have matching type IDs to the current filters
+    const validCrops = crops.filter(crop => {
+      let cropTypeID = crop.typeID.split(";");
+      for (let i = 0; i < cropTypeID.length; i++) {
+        if (currentCropFilters.includes(cropTypeID[i])) {
+          return true;
+        }
+      } 
+      return false;
+    }).map(crop => crop.ID);
+    //console.log(validCrops);
+     
+    const validLivestock = livestock.filter(animal => {
+      if (currentLivestockFilters.includes(String(animal.typeID))) {
+        return true;
+      }
+      return false;
+    }).map(animal => animal.ID);
+    //console.log(validLivestock);
 
-    fetch(`/location/` + searchQuery + `&filters=${filters}`)
-      .then(response => response.json())
-      .then(data => {
-        setFarms(data);
-        mapFarmLocations(data);
-      });
+    const validFarms = currentFarmInfo.filter(currentFarm => {
+       let currentCrops = currentFarm.cropID.split(";");
+       let currentLivestock = currentFarm.livestockID.split(";");
+      
+      for (let i = 0; i < currentCrops.length; i++) {
+        if (validCrops.includes(parseInt(currentCrops[i]))) {
+          return true;
+        }
+      }
+      for (let i = 0; i < currentLivestock.length; i++) {
+        if (validLivestock.includes(parseInt(currentLivestock[i]))) {
+          return true;
+        }
+      }
+      return false;
+    }).map(currentFarm => currentFarm.ID);
+    setValidFarms(validFarms);
+    console.log(validFarms);
 
-  }, [searchQuery, currentFilters]);
-  */
+    // Check farmcurrent's crop & livestock IDs to match against valid crop/livestock IDs
+    // Make filterd list of farm IDs
+
+
+  }, [searchQuery, crops, livestock, currentFarmInfo,
+    currentCropFilters, currentLivestockFilters]);
+
 
   /*
     * Sets new longitude, latitude, zoom on move
@@ -166,6 +221,127 @@ export default function FarmsMap () {
   }   
 
   /*
+    * Gets crop categories for farm search filter
+    */
+  const getCropFilters = async () => {
+    const response = await fetch(`/cropType/`);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    let cropFilters = records.map(cropType => (
+      {
+        label: cropType.type,
+        value: cropType.cropTypeID
+      }
+    ));
+    
+    //console.log(cropFilters);
+    return cropFilters;
+  }
+
+  /*
+  * Gets livestock categories for farm search filter
+  */
+  const getLivestockFilters = async () => {
+    const response = await fetch(`/livestockType/`);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    let livestockFilters = records.map(livestockType => (
+      {
+        label: livestockType.livestockType,
+        value: livestockType.livestockTypeID
+      }
+    ));
+    return livestockFilters;
+  }
+
+  /*
+  * Gets all crops for farm filtering
+  */
+  const getCrops = async () => {
+    const response = await fetch(`/crop/`);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    let crops = records.map(cropType => (
+      {
+        ID: cropType.cropID,
+        name: cropType.name,
+        typeID: cropType.cropTypeID
+      }
+    ));
+    
+    //console.log(crops);
+    return crops;
+  }
+
+  /*
+  * Gets all livestock for farm filtering
+  */
+  const getLivestock = async () => {
+    const response = await fetch(`/livestock/`);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    let livestock = records.map(livestockType => (
+      {
+        ID: livestockType.id,
+        name: livestockType.name,
+        typeID: livestockType.livestockTypeID
+      }
+    ));
+    
+    //console.log(livestock);
+    return livestock;
+  }
+
+  /*
+  * Gets all livestock for farm filtering
+  */
+  const getCurrentFarmInfo = async () => {
+    const response = await fetch(`/currentFarm/`);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    let currentFarmInfo = records.map(currentFarm => (
+      {
+        ID: currentFarm.farmCurrentID,
+        cropID: currentFarm.cropID,
+        livestockID: currentFarm.livestockID
+      }
+    ));
+    
+    console.log(currentFarmInfo);
+    return currentFarmInfo;
+  }
+
+  /*
   * Reorients map to focus on selected farm
   * Code adapted from (https://docs.mapbox.com/help/tutorials/building-a-store-locator/)
   */
@@ -181,8 +357,11 @@ export default function FarmsMap () {
   * Resets multiselectexpanded state if user clicks away
   * Code by Cam Bass (https://cambass.medium.com/building-a-category-filter-with-reactjs-mern-stack-193f46ff385)
   */
-  const handleClickAway = () => {
-    setMultiSelectExpanded(false)
+  const handleClickAway = (filterType) => {
+    if (filterType === "crop")
+      setCropMultiSelectExpanded(false)
+    else if (filterType === "livestock")
+      setLivestockMultiSelectExpanded(false)
   }
 
   /*
@@ -223,13 +402,30 @@ export default function FarmsMap () {
 
         <SearchBar setQuery={setSearchQuery} />
         
-        <ClickAwayListener onClickAway={handleClickAway}>
+        <ClickAwayListener onClickAway={() => {handleClickAway("crop")}}>
           <div className='outer-filter-container'>
             <Filter 
-              multiSelectExpanded={multiSelectExpanded}
-              setMultiSelectExpanded={setMultiSelectExpanded}
-              currentFilters={currentFilters} 
-              setFilterQuery={setFilterQuery}
+              multiSelectExpanded={cropMultiSelectExpanded}
+              setMultiSelectExpanded={setCropMultiSelectExpanded}
+              currentFilters={currentCropFilters} 
+              setFilterQuery={setCropFilterQuery}
+              categoryName="Crop"
+              filterCategory={cropFilters}
+              setFilterCategory={setCropFilters}
+            />
+          </div>
+        </ClickAwayListener>
+
+        <ClickAwayListener onClickAway={() => {handleClickAway("livestock")}}>
+          <div className='outer-filter-container'>
+            <Filter 
+              multiSelectExpanded={livestockMultiSelectExpanded}
+              setMultiSelectExpanded={setLivestockMultiSelectExpanded}
+              currentFilters={currentLivestockFilters} 
+              setFilterQuery={setLivestockFilterQuery}
+              categoryName="Livestock"
+              filterCategory={livestockFilters}
+              setFilterCategory={setLivestockFilters}
             />
           </div>
         </ClickAwayListener>
@@ -240,9 +436,11 @@ export default function FarmsMap () {
         <div id='listings' className='listings'>
           {farms.filter(farm => {
             if (searchQuery === '' || 
-              farm.properties.name.toLowerCase().includes(searchQuery.toLowerCase())) {
-              return farm;
-            } 
+              farm.properties.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              validFarms.includes(farm.properties.id)) {
+              return true;
+            }
+            return false; 
           })
           .sort((a, b) => a.properties.name.localeCompare(b.properties.name)) // Sort farms alphabetically
           .map((farm) => (             
